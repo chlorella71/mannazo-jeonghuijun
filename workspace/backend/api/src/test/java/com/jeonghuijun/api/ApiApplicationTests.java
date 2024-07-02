eae6ff6eb4ac976c9a22196b0bedbcbe0991bd73
chlorella71 <chlorella71@naver.com>
2024-07-02 11:18:01 +0900

    first commit

---
+++ workspace/backend/api/src/test/java/com/jeonghuijun/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeonghuijun.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ workspace/backend/user/src/test/java/com/jeonghuijun/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeonghuijun.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
